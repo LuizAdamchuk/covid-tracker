dbe9209def482dc2a101cac74e18fa873d8d9169
LuizAdamchuk <adis_jr@hotmail.com>
2020-06-23 20:09:52 -0300

    Finished buildUp

---
+++ build/precache-manifest.2e4aa0c6654b082d5848e77bcfbc6915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569a03a6f5ae8ce240b58bbce22cfcbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3aaab418506cdb49d7f",
+    "url": "/static/js/2.bbe140f0.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.bbe140f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d903681092d1a3898454",
+    "url": "/static/js/main.f1ad4cbd.chunk.js"
+  },
+  {
+    "revision": "e663df2881e39a90859c",
+    "url": "/static/js/runtime-main.225c0f6f.js"
+  }
+]);
